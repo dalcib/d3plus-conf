0142f119fe411da1185f95c702be071125469732
dalcib <dalcib@gmail.com>
2019-08-19 03:37:25 +0530

    Updates

---
+++ precache-manifest.82c706a207f4ff836d82355d4fe81411.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa673556413b9d6538812894c2d7ecdf",
+    "url": "/d3plus-conf/index.html"
+  },
+  {
+    "revision": "f96f9a7427c4acd350f6",
+    "url": "/d3plus-conf/static/css/main.54d96a03.chunk.css"
+  },
+  {
+    "revision": "e0470fc2c008b8397b52",
+    "url": "/d3plus-conf/static/js/2.95601a51.chunk.js"
+  },
+  {
+    "revision": "f96f9a7427c4acd350f6",
+    "url": "/d3plus-conf/static/js/main.39429ecb.chunk.js"
+  },
+  {
+    "revision": "bc86658914f5e08f3430",
+    "url": "/d3plus-conf/static/js/runtime~main.fde48e10.js"
+  }
+]);
